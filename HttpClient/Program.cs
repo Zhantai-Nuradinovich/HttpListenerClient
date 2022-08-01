629b32a69881c739d29f119f5f588fc7fba570dd
Zhantai <jantai.nuradinovich@gmail.com>
2022-08-01 15:14:36 +0600

    initial commit

---
+++ HttpClient/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Http.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                CookieContainer cookies = new CookieContainer();
+                HttpClientHandler handler = new HttpClientHandler { CookieContainer = cookies };
+
+                using (var client = new HttpClient(handler))
+                {
+                    await GetMyNameAsync(client);
+
+                    await GetStatusCodeAsync(client, HttpStatusCode.NonAuthoritativeInformation);
+                    await GetStatusCodeAsync(client, HttpStatusCode.OK);
+                    await GetStatusCodeAsync(client, HttpStatusCode.Redirect);
+                    await GetStatusCodeAsync(client, HttpStatusCode.BadRequest);
+                    await GetStatusCodeAsync(client, HttpStatusCode.InternalServerError);
+
+                    await GetMyNameByHeaderAsync(client);
+
+                    await GetMyNameByCookiesAsync(client, cookies);
+
+                    await client.GetAsync("http://localhost:8888/Exit");
+                    Console.WriteLine("The app is stopped");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+
+        private static async Task GetMyNameAsync(HttpClient client)
+        {
+            HttpResponseMessage response = await client.GetAsync("http://localhost:8888/MyName");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Result of /MyName method is ====>    " + responseBody);
+        }
+
+        private static async Task GetStatusCodeAsync(HttpClient client, HttpStatusCode code)
+        {
+            string method = "";
+            switch (code)
+            {
+                case HttpStatusCode.NonAuthoritativeInformation:
+                    method = "Information";
+                    break;
+                case HttpStatusCode.OK:
+                    method = "Success";
+                    break;
+                case HttpStatusCode.Redirect:
+                    method = "Redirection";
+                    break;
+                case HttpStatusCode.BadRequest:
+                    method = "ClientError";
+                    break;
+                case HttpStatusCode.InternalServerError:
+                    method = "ServerError";
+                    break;
+            }
+
+            var fullUrl = "http://localhost:8888/" + method;
+            HttpResponseMessage response = await client.GetAsync(fullUrl);
+            var status = response.StatusCode;
+            Console.WriteLine($"Status code of /{method} method is ====>    " + status.ToString());
+        }
+
+        private static async Task GetMyNameByHeaderAsync(HttpClient client)
+        {
+            HttpResponseMessage response = await client.GetAsync("http://localhost:8888/MyNameByHeader");
+            response.EnsureSuccessStatusCode();
+            var responseBody = response.Headers.GetValues("X-MyName");
+            Console.WriteLine("Result of /MyNameByHeader method is ====>    " + string.Join(";", responseBody));
+        }
+
+        private static async Task GetMyNameByCookiesAsync(HttpClient client, CookieContainer cookies)
+        {
+            var uri = new Uri("http://localhost:8888/MyNameByCookies");
+            HttpResponseMessage response = await client.GetAsync(uri);
+            var result = cookies.GetCookies(uri).Cast<Cookie>().FirstOrDefault(x => x.Name == "MyName");
+            Console.WriteLine("Result of /MyNameByCookie method is ====>    " + result.Value);
+        }
+    }
+}
---
+++ HttpListenerClient/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Http.Listener
+{
+    class Program
+    {
+        static readonly string _myName = "ZHANTAI NURADINOV";
+        static async Task Main(string[] args)
+        {
+            using(var listener = new HttpListener())
+            {
+                var prefixes = new[] { "http://localhost:8888/" };
+
+                foreach (string s in prefixes)
+                    listener.Prefixes.Add(s);
+
+                await ListenAsync(listener);
+            }
+        }
+
+        public static async Task ListenAsync(HttpListener listener)
+        {
+            listener.Start();
+            Console.WriteLine("Listening...");
+            while (true)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                HttpListenerResponse response = context.Response;
+
+                var requestUrl = context.Request.RawUrl;
+                if (requestUrl == "/Exit")
+                {
+                    await SendAsync(response, "The app is stopped!", HttpStatusCode.OK);
+                    break;
+                }
+                else if (requestUrl == "/favicon.ico")
+                    continue;
+
+                string body = null;
+                HttpStatusCode code = HttpStatusCode.OK;
+                switch (requestUrl)
+                {
+                    case "/MyName":
+                        body = _myName;
+                        break;
+
+                    case "/Information":
+                        code = HttpStatusCode.NonAuthoritativeInformation; // If 1*** status code is passed - the request is awaited forever in the client-side
+                        body = code.ToString();
+                        break;
+
+                    case "/Success":
+                        body = code.ToString();
+                        break;
+
+                    case "/Redirection":
+                        code = HttpStatusCode.Redirect;
+                        body = code.ToString();
+                        break;
+
+                    case "/ClientError":
+                        code = HttpStatusCode.BadRequest;
+                        body = code.ToString();
+                        break;
+                    case "/ServerError":
+                        code = HttpStatusCode.InternalServerError;
+                        body = code.ToString();
+                        break;
+
+                    case "/MyNameByHeader":
+                        response.Headers.Add("X-MyName", _myName);
+                        body = _myName;
+                        break;
+
+                    case "/MyNameByCookies":
+                        response.AppendCookie(new Cookie() 
+                        { 
+                            Name = "MyName", 
+                            Value = _myName
+                        });
+                        body = _myName;
+                        break;
+                }
+
+                await SendAsync(response, body, code);
+            }
+
+            Console.WriteLine("Stopping...");
+            listener.Stop();
+        }
+
+        public static async Task SendAsync(HttpListenerResponse response, string body, HttpStatusCode code)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(body);
+            response.StatusCode = (int)code;
+            response.ContentType = "text/plain";
+            response.ContentLength64 = bytes.Length;
+            
+            await response.OutputStream.WriteAsync(bytes, 0, bytes.Length);
+            response.OutputStream.Close();
+        }
+    }
+}
